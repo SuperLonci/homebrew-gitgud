f992be94930a35a82478f00cae334c85c736bbb5
SuperLonci <55552282+SuperLonci@users.noreply.github.com>
2025-06-17 14:51:55 +0200

    Update to version 1.0.1

--- git-gud.rb
+++ git-gud.rb
@@ -1,11 +1,11 @@
 class GitGud < Formula
   desc "An alternative git help command: git gud"
   homepage "https://github.com/superlonci/git-gud"
-  version "1.0.0"
+  version "1.0.1"
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/SuperLonci/git-gud/releases/download/v.0/git-gud-macos-arm64"
-    sha256 "fee86f301d5541eb604462f1514d1b1c3f528df8521720cf2144f5697a780beb"
+    sha256 "2f59ddaaa2f541f71c389b0f2fa347ce311bcc3ed4225d4aaf4e1be17794d648"
 
     def install
       bin.install "git-gud-macos-arm64" => "git-gud"
@@ -13,7 +13,7 @@ class GitGud < Formula
 
   elsif OS.mac? && Hardware::CPU.intel?
     url "https://github.com/SuperLonci/git-gud/releases/download/v.0/git-gud-macos-amd64"
-    sha256 "65be2d983b916bb931035f2d7f6ed66689acc9c1c3c6330c6028c5bb116db27a"
+    sha256 "85ba678db9c04801be284185bad8da7422181493fbc857747ed8489e9adf36a2"
 
     def install
       bin.install "git-gud-macos-amd64" => "git-gud"
@@ -21,7 +21,7 @@ class GitGud < Formula
 
   elsif OS.linux?
     url "https://github.com/SuperLonci/git-gud/releases/download/v.0/git-gud-linux"
-    sha256 "da0324594413c0687d49afc6c73929443798e9f1fd15092635074a6564e26379"
+    sha256 "2a1fc18f4060bb233169860e9c2242af9c61a0a8a9873d9971c1ac94643dd1bb"
 
     def install
       bin.install "git-gud-linux" => "git-gud"
