c8102598d2e252cc507e49f5e1206caff9cd8e34
SuperLonci <55552282+SuperLonci@users.noreply.github.com>
2025-06-17 14:54:41 +0200

    Update git-gud.rb

--- git-gud.rb
+++ git-gud.rb
@@ -4,7 +4,7 @@ class GitGud < Formula
   version "1.0.1"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/SuperLonci/git-gud/releases/download/v.0/git-gud-macos-arm64"
+    url "https://github.com/SuperLonci/git-gud/releases/download/v1.0.1/git-gud-macos-arm64"
     sha256 "2f59ddaaa2f541f71c389b0f2fa347ce311bcc3ed4225d4aaf4e1be17794d648"
 
     def install
@@ -12,7 +12,7 @@ class GitGud < Formula
     end
 
   elsif OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/SuperLonci/git-gud/releases/download/v.0/git-gud-macos-amd64"
+    url "https://github.com/SuperLonci/git-gud/releases/download/v1.0.1/git-gud-macos-amd64"
     sha256 "85ba678db9c04801be284185bad8da7422181493fbc857747ed8489e9adf36a2"
 
     def install
@@ -20,7 +20,7 @@ class GitGud < Formula
     end
 
   elsif OS.linux?
-    url "https://github.com/SuperLonci/git-gud/releases/download/v.0/git-gud-linux"
+    url "https://github.com/SuperLonci/git-gud/releases/download/v1.0.1/git-gud-linux"
     sha256 "2a1fc18f4060bb233169860e9c2242af9c61a0a8a9873d9971c1ac94643dd1bb"
 
     def install
